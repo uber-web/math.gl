8fee7ec51947f66a67b9756614c4ac12e2088bd9
Ib Green <ib@uber.com>
2018-03-21 17:14:22 -0700

    v1.1.0-alpha.1

--- test/node-dist.js
+++ test/node-dist.js
@@ -24,7 +24,7 @@ require('../aliases');
 // Registers an alias for this module
 const path = require('path');
 const moduleAlias = require('module-alias');
-moduleAlias.addAlias('math.gl', path.resolve('./dist'));
+moduleAlias.addAlias('math.gl', path.resolve('./dist/es6'));
 
 // Run the tests
 require('./index');
