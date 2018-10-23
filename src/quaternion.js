d64646b826fdacb0c88c24635c4b5677d5fc3b9d
Xintong Xia <xintong@uber.com>
2018-10-23 16:43:50 +0200

    Support quaternion to euler convertion

--- src/quaternion.js
+++ src/quaternion.js
@@ -20,6 +20,7 @@
 
 import MathArray from './lib/math-array';
 import {checkNumber} from './lib/common';
+import Euler from './euler';
 
 // gl-matrix is too big. Cherry-pick individual imports from stack.gl version
 /* eslint-disable camelcase */
@@ -262,5 +263,24 @@ export default class Quaternion extends MathArray {
   slerp({start = IDENTITY_QUATERNION, target, ratio}) {
     quat_slerp(this, start, target, ratio);
     return this.check();
+  }
+
+  toEuler() {
+    const {w, x, y, z} = this;
+    const ysqr = y * y;
+    const t0 = -2.0 * (ysqr + z * z) + 1.0;
+    const t1 = +2.0 * (x * y + w * z);
+    let t2 = -2.0 * (x * z - w * y);
+    const t3 = +2.0 * (y * z + w * x);
+    const t4 = -2.0 * (x * x + ysqr) + 1.0;
+
+    t2 = t2 > 1.0 ? 1.0 : t2;
+    t2 = t2 < -1.0 ? -1.0 : t2;
+
+    const roll = Math.atan2(t3, t4);
+    const pitch = Math.asin(t2);
+    const yaw = Math.atan2(t1, t0);
+
+    return new Euler(roll, pitch, yaw, Euler.RollPitchYaw);
   }
 }
