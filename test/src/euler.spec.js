5a6b58d6644647ca47941f48e4739b3412a66a1c
Xintong Xia <xintong@uber.com>
2018-11-27 16:35:06 +0100

    update tests

--- test/src/euler.spec.js
+++ test/src/euler.spec.js
@@ -11,10 +11,12 @@ test('Euler#construct and Array.isArray check', t => {
   t.end();
 });
 
+/* eslint-disable */
 test('Euler.toQuaternion', t => {
   const eulers = [
+    new Euler(90 / 180 * Math.PI, -89 / 180 * Math.PI, - 180 / 180 * Math.PI, Euler.RollPitchYaw),
     new Euler(30 / 180 * Math.PI, 45 / 180 * Math.PI, 90 / 180 * Math.PI, Euler.RollPitchYaw),
-    new Euler(11 / 180 * Math.PI, 67 / 180 * Math.PI, 45 / 180 * Math.PI, Euler.RollPitchYaw),
+    new Euler(11 / 180 * Math.PI, 67 / 180 * Math.PI, 45 / 180 * Math.PI, Euler.RollPitchYaw)
   ];
   const quaternions = eulers.map(e => e.toQuaternion());
   t.ok(quaternions.every((q, i) => equals(q.toEuler(), eulers[i])));
