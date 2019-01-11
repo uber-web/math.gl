012ff1812eecf011484bb82b98379b555f7e6045
Ib Green <ib@uber.com>
2019-01-10 17:11:08 -0800

    Update scripts

--- src/euler.js
+++ src/euler.js
@@ -109,6 +109,26 @@ export default class Euler extends MathArray {
     }
   }
 
+  fromQuaternion(quaternion) {
+    const [w, x, y, z] = quaternion;
+    const ysqr = y * y;
+    const t0 = -2.0 * (ysqr + z * z) + 1.0;
+    const t1 = +2.0 * (x * y + w * z);
+    let t2 = -2.0 * (x * z - w * y);
+    const t3 = +2.0 * (y * z + w * x);
+    const t4 = -2.0 * (x * x + ysqr) + 1.0;
+
+    t2 = t2 > 1.0 ? 1.0 : t2;
+    t2 = t2 < -1.0 ? -1.0 : t2;
+
+    const roll = Math.atan2(t3, t4);
+    const pitch = Math.asin(t2);
+    const yaw = Math.atan2(t1, t0);
+
+    return new Euler(roll, pitch, yaw, Euler.RollPitchYaw);
+  }
+
+
   // If copied array does contain fourth element, preserves currently set order
   copy(array) {
     for (let i = 0; i < 3; ++i) {
--- src/quaternion.js
+++ src/quaternion.js
@@ -20,7 +20,6 @@
 
 import MathArray from './lib/math-array';
 import {checkNumber} from './lib/common';
-import Euler from './euler';
 
 import * as quat from 'gl-matrix/quat';
 
@@ -251,24 +250,5 @@ export default class Quaternion extends MathArray {
   slerp({start = IDENTITY_QUATERNION, target, ratio}) {
     quat.slerp(this, start, target, ratio);
     return this.check();
-  }
-
-  toEuler() {
-    const {w, x, y, z} = this;
-    const ysqr = y * y;
-    const t0 = -2.0 * (ysqr + z * z) + 1.0;
-    const t1 = +2.0 * (x * y + w * z);
-    let t2 = -2.0 * (x * z - w * y);
-    const t3 = +2.0 * (y * z + w * x);
-    const t4 = -2.0 * (x * x + ysqr) + 1.0;
-
-    t2 = t2 > 1.0 ? 1.0 : t2;
-    t2 = t2 < -1.0 ? -1.0 : t2;
-
-    const roll = Math.atan2(t3, t4);
-    const pitch = Math.asin(t2);
-    const yaw = Math.atan2(t1, t0);
-
-    return new Euler(roll, pitch, yaw, Euler.RollPitchYaw);
   }
 }
