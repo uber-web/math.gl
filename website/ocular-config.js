763437c4a8956ca3d00b078158527367616f3c0b
Jerome Cukier <jerome@uber.com>
2019-02-05 15:56:11 -0800

    changes to file for better docs generation

--- website/ocular-config.js
+++ website/ocular-config.js
@@ -14,24 +14,40 @@ module.exports = {
   PROJECT_NAME: 'math.gl',
   PROJECT_ORG: 'uber-web',
   PROJECT_URL: 'https://github.com/uber-web/math.gl',
-  PROJECT_DESC: 'a math/3d library',
+  PROJECT_DESC: 'A 3D/WebGL math library',
   WEBSITE_PATH: '/website/',
 
   FOOTER_LOGO: '',
 
   HOME_PATH: '/',
-  HOME_HEADING: 'a math/3d library',
+  HOME_HEADING: 'A 3D/WebGL math library',
   HOME_RIGHT: null,
   HOME_BULLETS: [
     {
-      text: 'Designed for React',
-      desc: 'Seamless integration.',
+      text: 'Array-based Classes',
+      desc: 'All math.gl classes (like Vector3, Matrix4) are subclasses of the built-in JavaScript Array class. This means that they can be used directly with any Javascript function that e.g. expects plain JavaScript Array arguments for Vectors, which is increasingly common.',
       img: 'images/icon-react.svg'
     },
     {
-      text: 'Totally ready for production',
+      text: 'Debug Friendly',
+      desc: 'JavaScript math can be frustrating to code and debug. math.gl offers optional error checking after every math operation which makes quick work of locating coding errors and bad data. Also, strong "printing support" (toString) simplifies debugging.',
       img: 'images/icon-layers.svg'
-    }
+    },
+    {
+      text: 'WebGL Support',
+      desc: 'Matrices are stored internally in the format required by WebGL (array of contiguous values in column-major order), while exposing the more "natural" row-major API to the JavaScript programmer (e.g. through accessors, printing using toString() etc).',
+      img: 'images/icon-layers.svg'
+    },
+    {
+      text: 'Documentation',
+      desc: 'Some JavaScript 3D math libraries come with reference documentation only. If you are new to 3D programming it can be hard to know where to start. math.gl comes with articles that try to show you the big picture and get you quickly up-to-speed on the mathematical concepts and the math.gl classes that support them.',
+      img: 'images/icon-layers.svg'
+    },
+    {
+      text: 'Size Conscious',
+      desc: 'A math library can quickly get big as a various classed and functions keep getting added. But for many 3D applications, only a few basic operations are necessary. math.gl has made a choices to restrict itself to a set of classes and functions that are likely to be most important in WebGL applications. The intention is to position math.gl as a "mid-size" 3D math library: reasonably full featured, but small enough that unless you are targeting a very small final bundle for your application, its size should not be a big concern.',
+      img: 'images/icon-layers.svg'
+    }  
   ],
 
   ADDITIONAL_LINKS: [],
