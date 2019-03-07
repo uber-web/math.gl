cb66bf4f60a335ee12a3cdbb2c4ed238d667493b
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-07 15:33:46 -0800

    upgrade ocular (#46)

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const getWebpackConfig = require('ocular-dev-tools/config/webpack.config');
+
+module.exports = (env = {}) => {
+  const config = getWebpackConfig(env);
+
+  switch (env.mode) {
+    case 'size':
+      config.entry = fs.readdirSync('./test/size').reduce((entries, filename) => {
+        entries[filename.replace('.js', '')] = `./test/size/${filename}`;
+        return entries;
+      }, {});
+      break;
+  }
+
+  return config;
+};
