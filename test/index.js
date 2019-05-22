725f1cefb432d1c9794f0c5ad747624a64f6fbf4
Ib Green <ib@uber.com>
2019-05-22 16:34:50 -0700

    Bump ocular-dev-tools

--- test/index.js
+++ test/index.js
@@ -18,4 +18,5 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import '../modules/core/test';
+require('reify');
+require('../modules/core/test');
