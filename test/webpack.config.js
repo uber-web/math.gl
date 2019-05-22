8baf158780c7f60fe511353291e66aa8f719c08e
Ib Green <ib@uber.com>
2019-05-22 16:34:50 -0700

    Bump ocular-dev-tools

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,3 +1,124 @@
+const {resolve, join} = require('path');
+const webpack = require('webpack');
+
+const rootDir = join(__dirname, '..');
+const libSources = join(rootDir, 'src');
+
+const ALIASES = require('ocular-dev-tools/config/ocular.config')({
+  root: resolve(__dirname, '..')
+}).aliases;
+
+// Otherwise modules imported from outside this directory does not compile
+// Seems to be a Babel bug
+// https://github.com/babel/babel-loader/issues/149#issuecomment-191991686
+const BABEL_CONFIG = {
+  presets: ['es2015', 'react', 'stage-2'].map(name => require.resolve(`babel-preset-${name}`)),
+  plugins: [
+    // website uses decorators
+    require.resolve('babel-plugin-transform-decorators-legacy')
+    // resolve(__dirname, '../modules/babel-plugin-inline-gl-constants')
+  ]
+};
+
+const COMMON_CONFIG = {
+  mode: 'development',
+
+  entry: ['./src/main'],
+
+  output: {
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        // Compile ES2015 using babel
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: BABEL_CONFIG,
+        include: [resolve('..'), libSources],
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [resolve(__dirname, './node_modules'), resolve(__dirname, '../node_modules')],
+
+    alias: Object.assign({}, ALIASES)
+  },
+
+  node: {
+    fs: 'empty'
+  },
+
+  plugins: []
+};
+
+const addDevConfig = config => {
+  config.module.rules.push({
+    // Unfortunately, webpack doesn't import library sourcemaps on its own...
+    test: /\.js$/,
+    use: ['source-map-loader'],
+    enforce: 'pre'
+  });
+
+  return Object.assign(config, {
+    devtool: 'source-maps',
+
+    plugins: config.plugins.concat([
+      // new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ])
+  });
+};
+
+const addProdConfig = config => {
+  return Object.assign(config, {
+    mode: 'production',
+
+    output: {
+      path: resolve(__dirname, './dist'),
+      filename: 'bundle.js'
+    }
+  });
+};
+
+// Different versions of webpack supply objects or arrays
+const getEnv = env => {
+  if (Array.isArray(env)) {
+    return env.reduce((key, obj) => {
+      obj[key] = true;
+    }, {});
+  }
+
+  if (typeof env === 'string') {
+    return {[env]: true};
+  }
+
+  return env || {};
+};
+
+module.exports = env => {
+  env = getEnv(env);
+
+  let config = COMMON_CONFIG;
+
+  if (env.local) {
+    config = addDevConfig(config);
+  }
+
+  if (env.prod) {
+    config = addProdConfig(config);
+  }
+
+  // Enable to debug config
+  // console.warn(JSON.stringify(config, null, 2));
+
+  return config;
+};
+
+/*
 // Copyright (c) 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -120,3 +241,4 @@ module.exports = env => {
     plugins: [new UglifyJsPlugin(), new BundleAnalyzerPlugin()]
   });
 };
+*/
