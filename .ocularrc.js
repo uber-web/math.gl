a797c39f0c6ac16cd41a44e0f5f12e990b0cd218
Ib Green <ib@unfolded.ai>
2021-04-09 16:26:35 -0700

    chore: upgrade ocular

---
+++ .ocularrc.js
@@ -0,0 +1,21 @@
+const {resolve} = require('path');
+
+module.exports = {
+  lint: {
+    // TODO - comment out while getting typescript to work
+    paths: ['dev-docs', 'docs', 'modules', 'examples'], // test', 'website', 'examples'],
+    extensions: ['js', 'md']
+  },
+
+  aliases: {
+    test: resolve(__dirname, 'test')
+  },
+
+  entry: {
+    test: 'test/node.js',
+    'test-browser': 'test/browser.js',
+    bench: 'test/bench/node.js',
+    'bench-browser': 'test/bench/browser.js',
+    size: 'test/size/import-nothing.js'
+  }
+};
