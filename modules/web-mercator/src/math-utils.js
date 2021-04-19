f89ca098d6885c62dfbae3370a073ca17cf1f9d7
Daniil <daniil.ryabinin@actionengine.com>
2021-04-19 18:31:51 +0300

    ESLint fix in math-utils.js file

--- modules/web-mercator/src/math-utils.js
+++ modules/web-mercator/src/math-utils.js
@@ -21,5 +21,8 @@ export function lerp(start, end, step) {
   return step * end + (1 - step) * start;
 }
 
+function ieLog2(x) {
+  return Math.log(x) * Math.LOG2E;
+}
 // Handle missing log2 in IE 11
-export const log2 = Math.log2 || function(x){ return Math.log(x) * Math.LOG2E; };
+export const log2 = Math.log2 || ieLog2;
