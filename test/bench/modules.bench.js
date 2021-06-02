5eb25ca83471ffae76591a23cbc865b002135deb
Ib Green <ib@unfolded.ai>
2021-06-02 12:32:08 -0700

    chore: Use babel-register to run tests and bench (#236)

--- test/bench/modules.bench.js
+++ test/bench/modules.bench.js
@@ -1,7 +1,7 @@
-import coreBench from '../modules/core/test/bench';
-import geospatialBench from '../modules/geospatial/test/bench';
-import cullingBench from '../modules/culling/test/bench';
-import polygonBench from '../modules/polygon/test/bench';
+import coreBench from '../../modules/core/test/bench';
+import geospatialBench from '../../modules/geospatial/test/bench';
+import cullingBench from '../../modules/culling/test/bench';
+import polygonBench from '../../modules/polygon/test/bench';
 
 export default function addBenchmarks(suite, addReferenceBenchmarks) {
   coreBench(suite, addReferenceBenchmarks);
