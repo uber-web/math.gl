d6295c91b8cd88585f8b4463c9c7ef62bf47e891
Ib Green <ib@unfolded.ai>
2023-02-28 16:26:04 -0500

    wip

--- modules/types/src/index.ts
+++ modules/types/src/index.ts
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 export type {TypedArray, NumericArray, NumberArray} from './array-types';
 export {isTypedArray, isNumericArray} from './is-array';
+=======
+export {TypedArray, TypedArrayConstructor, NumberArray, NumericArray} from './number-array';
+>>>>>>> 869c8ba (feat(core): Global config)
