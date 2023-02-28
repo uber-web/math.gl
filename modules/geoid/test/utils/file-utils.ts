67d68f89f3456aade661c8387a958ee35a56b961
Ib Green <ib@unfolded.ai>
2023-02-28 18:28:55 -0500

    tests

--- modules/geoid/test/utils/file-utils.ts
+++ modules/geoid/test/utils/file-utils.ts
@@ -2,11 +2,11 @@ import fs from 'fs';
 
 export async function openFile(filePath: string): Promise<Uint8Array | null> {
   let data: Uint8Array | null = null;
-  if (typeof fetch !== 'undefined') {
+  if (fs) {
+    data = await fs.promises.readFile(filePath);
+  } else if (typeof fetch !== 'undefined') {
     const request = await fetch(filePath);
     data = new Uint8Array(await request.arrayBuffer());
-  } else if (fs) {
-    data = await fs.promises.readFile(filePath);
   }
   return data;
 }
